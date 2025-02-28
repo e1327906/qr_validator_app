2dc8cb39d3fd8428d20339ae25911830461b12b3
zawminnthan <zawminnthan09@gmail.com>
2025-02-28 15:34:08 +0800

    added aws cognito D:\dev\flutter_windows_3.13.9-stable\flutter

---
+++ lib/amplifyconfiguration.dart
@@ -0,0 +1,45 @@
+const amplifyconfig = '''{
+    "UserAgent": "aws-amplify-cli/2.0",
+    "Version": "1.0",
+    "auth": {
+        "plugins": {
+            "awsCognitoAuthPlugin": {
+                "UserAgent": "aws-amplify-cli/0.1.0",
+                "Version": "0.1.0",
+                "IdentityManager": {
+                    "Default": {}
+                },
+                "CognitoUserPool": {
+                    "Default": {
+                        "PoolId": "ap-southeast-1_ytxWGEGkd",
+                        "AppClientId": "228nmabt8o2u6e32keul6v5o0v",
+                        "Region": "ap-southeast-1"
+                    }
+                },
+                "Auth": {
+                    "Default": {
+                        "authenticationFlowType": "USER_SRP_AUTH",
+                        "socialProviders": [],
+                        "usernameAttributes": [
+                            "EMAIL"
+                        ],
+                        "signupAttributes": [
+                            "EMAIL"
+                        ],
+                        "passwordProtectionSettings": {
+                            "passwordPolicyMinLength": 8,
+                            "passwordPolicyCharacters": []
+                        },
+                        "mfaConfiguration": "OFF",
+                        "mfaTypes": [
+                            "SMS"
+                        ],
+                        "verificationMechanisms": [
+                            "EMAIL"
+                        ]
+                    }
+                }
+            }
+        }
+    }
+}''';
--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,53 @@
+// main.dart
 import 'package:flutter/material.dart';
 import 'package:global_configuration/global_configuration.dart';
 import 'package:qr_validator_app/pages/home_page.dart';
+import 'package:amplify_flutter/amplify_flutter.dart';
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'package:amplify_authenticator/amplify_authenticator.dart';
+import 'amplifyconfiguration.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await GlobalConfiguration().loadFromAsset("app_settings");
+  await _configureAmplify();
   runApp(const MyApp());
+}
+
+Future<void> _configureAmplify() async {
+  try {
+    // Add Auth plugin
+    final auth = AmplifyAuthCognito();
+    await Amplify.addPlugin(auth);
+
+    // Configure Amplify
+    await Amplify.configure(amplifyconfig);
+    print('Successfully configured Amplify');
+  } catch (e) {
+    print('Error configuring Amplify: $e');
+  }
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'QR Validator',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueGrey),
-        useMaterial3: true,
+    return Authenticator(
+      // Set initial step to sign in
+      initialStep: AuthenticatorStep.signIn,
+      // Disable sign up functionality by setting it to null
+      signUpForm: null,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        builder: Authenticator.builder(),
+        title: 'QR Validator',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueGrey),
+          useMaterial3: true,
+        ),
+        home: const HomePage(),
       ),
-      home: const HomePage(),
     );
   }
 }
